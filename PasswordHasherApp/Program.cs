e8a6babda9189b3ed3d94d34b125391e69cac8d1
Asm Sayem <Asm Sayem@DESKTOP-5C486UG>
2025-02-10 18:46:00 +0600

    Add project files.

---
+++ PasswordHasherApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordHasherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = "123456";
+
+            var hasher = new PasswordHasher<object>();
+            var hashedPassword = hasher.HashPassword(null, password);
+
+            Console.WriteLine($"Hashed Password for {password} is : {hashedPassword}");
+            Console.ReadLine();
+        }
+    }
+}
